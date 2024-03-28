2f904ff2d2c2c8fb83e1827c22f3a344c5cc77df
Abhishek More <abhimore74@gmail.com>
2024-03-28 03:08:37 -0500

    transparent background - now matches terminal theme

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.cmd("hi Normal ctermbg=none guibg=none")
+
 -- require("flit").setup({
 --   keys = { f = "f", F = "F", t = "t", T = "T" },
 --   -- A string like "nv", "nvo", "o", etc.
