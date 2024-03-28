f49b80ddf4befcdd3ea260d2685a2213fdae34f1
Abhishek More <abhimore74@gmail.com>
2024-03-28 03:16:24 -0500

    nvm i dont like it

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.cmd("hi Normal ctermbg=none guibg=none")
-
 -- require("flit").setup({
 --   keys = { f = "f", F = "F", t = "t", T = "T" },
 --   -- A string like "nv", "nvo", "o", etc.
